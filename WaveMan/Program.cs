eb8cf553653cecbbcc27444564d0f35297d61c52
kristianbraathen <97292975+kristianbraathen@users.noreply.github.com>
2022-06-21 13:56:05 +0200

    Add project files.

---
+++ WaveMan/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace WaveMan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stage = new Stage();
+
+            while (true)
+            {
+                stage.ShowAndMove();
+                Console.CursorLeft = 0;
+                Console.CursorTop = 0;
+                Thread.Sleep(200);
+            }
+        }
+    
+    }
+}
---
+++ WaveMan/Stage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveMan
+{
+    public class Stage
+    {
+        private readonly Man[] _men = new Man[]
+        {
+            new Man(5,5,-1,0), new Man(6,5,1,0), new Man(9,5,1,0),  new Man(15,6,0,10){IsLeftHanded = true},new Man(10,6,0,10){IsLeftHanded = true},new Man(5,6,0,10){IsLeftHanded = true}
+        };
+
+        public void ShowAndMove()
+        {
+            Console.Clear();
+            foreach (var person in _men)
+            {
+                person.Show();
+                person.Move();
+            }
+
+        }
+    }
+}
